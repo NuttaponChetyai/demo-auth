acb73a561af5c20bcbbf70e1e00790e824fb3b90
Nuttapon Chetuyai <Nuttapon.Chetuyai@krungsrinimble.com>
2024-03-19 12:05:35 +0700

    add modal

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserForm } from '../models/user-form';
 import { AuthService } from '../services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,7 @@ import { AuthService } from '../services/auth.service';
 export class RegisterComponent {
   form: UserForm = new UserForm();
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private dialog: MatDialog) {}
 
   onSubmit() {
     const { username, password, email } = this.form;
@@ -18,12 +20,15 @@ export class RegisterComponent {
     console.log('email : ' + email);
     console.log('password : ' + password);
     this.authService.register(username, email, password).subscribe({
-      next: data => {
+      next: (data) => {
         console.log(data);
       },
-      error: err => {
+      error: (err) => {
         console.error(err);
-      }
+        this.dialog.open(ErrorDialogComponent, {
+          data: { message: err.message },
+        });
+      },
     });
   }
 }
