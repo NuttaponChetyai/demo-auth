d494391473d6268ac135801fe8294a97682b88ef
Nuttapon Chetuyai <Nuttapon.Chetuyai@krungsrinimble.com>
2024-03-19 13:59:00 +0700

    work session storage

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { UserForm } from '../models/user-form';
 import { AuthService } from '../services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-register',
@@ -12,13 +13,18 @@ import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 export class RegisterComponent {
   form: UserForm = new UserForm();
 
-  constructor(private authService: AuthService, private dialog: MatDialog) {}
+  constructor(
+    private authService: AuthService,
+    private storageService: StorageService,
+    private dialog: MatDialog
+  ) {}
 
   onSubmit() {
     const { username, password, email } = this.form;
     console.log('username : ' + username);
     console.log('email : ' + email);
     console.log('password : ' + password);
+    this.storageService.saveUser({ username, email, password: "******" });
     this.authService.register(username, email, password).subscribe({
       next: (data) => {
         console.log(data);
